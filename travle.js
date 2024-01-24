65658ffc45332c5aeb7ecb65ff91734789326083
Tarun <tarunsagwal38@gmail.com>
2024-01-24 15:21:49 +0530

    js not work

--- travle.js
+++ travle.js
@@ -1,66 +1,52 @@
 let count=0
-let run=()=>{
-    let imga=document.querySelectorAll('.back_img')
-    for(var im=0; im<imga.length; im++){
-        imga[im].style.opacity='0';
+let main=()=>{
+  let img=document.querySelectorAll(".back_img");
+  for(var imge=0; imge<img.length; imge++){
+    img[imge].style.opacity='0';
+    // count++;
+  }
+  if(count>img.length){
+    count=1;
+  }
+  img[count-1].style.opacity='1';
+  // console.log(count);
+  let slider=document.querySelectorAll(".fram_img");
+  slider.forEach((elem,index)=>{
+    elem.style.left=`${index*100}%`;
+  })
+  let scroll=()=>{
+    slider.forEach((fram) => {
+      fram.style.transform=`translateX(-${count*100}%)`
+    });
+  }
+nxt=()=>{
+    scroll();
+    if(count<=slider.length){
+        count-1;
+     }
+     //if(count==-1){
+    //     count=0;
+    //     scroll();
+    // }
+    console.log(count+"hii")
+  }
+  left=()=>{
+    scroll();
+    if(count>=slider.length){
+      
+      count--;
+      main();
+      nxt();
     }
-    count++
-    if(count>imga.length){
-        count=1;
-    }
-    imga[count-1].style.opacity='1';
+  }
+// pre=()=>{
+//     scroll();
+//     console.log(count+"hee")
+// }
 }
-console.log(count);
-let counte=0;
-let con=()=>{
-    let cor=document.querySelectorAll('.dot')
-    for(var co=0; co<cor.length; co++){
-        cor[co].style.transform=`scale(.1)`;
-    }
-    counte++
-    console.log(counte);
-    if(counte>cor.length){
-        counte=1;
-    }
-    cor[counte-1].style.transform=`scale(1.2)`
+let right=()=>{
+  count++;
+  main();
+  nxt();
 }
-setInterval(con,3000);
-setInterval(run,3000);
-var app = document.getElementById('app');
-var typewriter = new Typewriter(app, {
-  loop: true,
-  delay: 75,
-});
 
-typewriter
-// .typeString('travle place')
-//   .pauseFor(2000)
-//   .deleteChars(12)
-  .typeString('kerala')
-  .pauseFor(2000)
-  .deleteChars(6)
-  .typeString('indonesia')
-  .pauseFor(2000)
-  .deleteChars(9)
-  .typeString('thailand')
-  .pauseFor(1000)
-  .deleteChars(8)
-  .typeString('bali')
-  .pauseFor(1000)
-  .deleteChars(4)
-//   .pauseFor(1000)
-  .start();
-  let coute=0;
-  let par=()=>{
-    let parh=document.querySelectorAll('.graph');
-    for(let pgr=0; pgr<=parh.length; pgr++){
-        parh[pgr].style.transform=`translateY(-100px)`
-    }
-    coute++;
-    if(coute>parh.length){
-        coute=1
-    }
-    parh[coute-1].style.transform=`translateY(-100px)`
-}
-setInterval(par,3000)
-  
